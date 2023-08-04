30e08bd03b24781e1edf8945b82b4b9bb40f7c6c
mixnuts07 <french0719@gmail.com>
2023-08-04 23:24:29 +0900

    add todo controller

--- src/app/modules/todo/controllers/TodoController.ts
+++ src/app/modules/todo/controllers/TodoController.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post } from '@nestjs/common';
 
 @Controller('todo')
 export class TodoController {
   @Get()
-  findAll(): string {
+  async getAllTodo(): Promise<string> {
+    // 登録したTODOリストを全て取得する
     return 'Hello Nest World';
+  }
+  @Post()
+  registerTodo(): string {
+    // TODOを1つ登録する
+    return 'post your todo';
+  }
+  @Delete()
+  deleteTodo(): string {
+    return 'delete you todo';
   }
 }
