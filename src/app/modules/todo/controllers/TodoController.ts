4204730518603ed19dda6dcb94ecbdbd9772ab44
mixnuts07 <french0719@gmail.com>
2023-08-08 23:58:43 +0900

    add create todo

--- src/app/modules/todo/controllers/TodoController.ts
+++ src/app/modules/todo/controllers/TodoController.ts
@@ -1,25 +1,14 @@
-import { Controller, Delete, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post } from '@nestjs/common';
 import { TodoService } from 'src/app/core/services/TodoService';
 import { CreateTodoDto } from '../dto/CreateTodoDto';
+import { Todo } from 'src/app/core/entities/todo/Todo';
 
 @Controller('todo')
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
-  @Get()
-  async getAllTodo(): Promise<string> {
-    // 登録したTODOリストを全て取得する
-    return 'Hello Nest World';
-  }
-
   @Post()
-  registerTodo(): void {
-    // TODOを1つ登録する
-    this.todoService.create(CreateTodoDto);
-  }
-
-  @Delete()
-  deleteTodo(): string {
-    return 'delete you todo';
+  async create(@Body() createTodoDto: CreateTodoDto): Promise<Todo> {
+    return this.todoService.create(createTodoDto);
   }
 }
