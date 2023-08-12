f49eb9984fab49a4a095ca136342b2bd4cf93f77
mixnuts07 <french0719@gmail.com>
2023-08-12 16:04:56 +0900

    add repo

--- src/app/todos/todos.repository.ts
+++ src/app/todos/todos.repository.ts
@@ -1,15 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { Todo } from './entities/Todo';
 import { Repository } from 'typeorm';
-import { todoDataSource } from 'src/app/todos/database.providers';
 
 @Injectable()
 export class ToDoRepository {
   private readonly repository: Repository<Todo>;
-
-  constructor() {
-    this.repository = todoDataSource.getRepository(Todo);
-  }
 
   async save(todo: Todo): Promise<Todo> {
     return await this.repository.save(todo);
